8d0144081ee5ee6226256a1e0cbcbc8c4441a2ef
waltherx <waltermj2012@gmail.com>
2023-06-15 01:12:00 -0400

    Update clients.ts

--- src/app/api/clients.ts
+++ src/app/api/clients.ts
@@ -8,7 +8,7 @@ export default async function handler(
     res: NextApiResponse
 ) {
     const prisma = new PrismaClient();
-    const client = await prisma.cliente.findMany();
+    const client = await prisma.user.findMany();
     console.log("aqui", client);
     return res.status(200).json(client);
 }
